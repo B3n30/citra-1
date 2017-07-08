a033b8bb9151a28fab41f874ca3dd0a563979d8f
B3n30 <benediktthomas@gmail.com>
2017-07-08 15:50:59 +0200

    Added Packet class for serialization

--- src/network/packet.cpp
+++ src/network/packet.cpp
@@ -13,12 +13,12 @@
 
 #ifndef htonll
 #define htonll(x)                                                                                  \
-    ((1 == htonl(1)) ? (x) : ((uint64_t)htonl((x)&0xFFFFFFFF) << 32) | htonl((x) >> 32))
+    ((1 == htonl(1)) ? (x) : ((u64)htonl((x)&0xFFFFFFFF) << 32) | htonl((x) >> 32))
 #endif
 
 #ifndef ntohll
 #define ntohll(x)                                                                                  \
-    ((1 == ntohl(1)) ? (x) : ((uint64_t)ntohl((x)&0xFFFFFFFF) << 32) | ntohl((x) >> 32))
+    ((1 == ntohl(1)) ? (x) : ((u64)ntohl((x)&0xFFFFFFFF) << 32) | ntohl((x) >> 32))
 #endif
 
 namespace Network {
