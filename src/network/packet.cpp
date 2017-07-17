54adaaf88e2a86cb7e9f4bc1b4cd5c8ffab7304a
B3n30 <benediktthomas@gmail.com>
2017-07-17 12:09:25 +0200

    Network: Set and send the game information over enet

--- src/network/packet.cpp
+++ src/network/packet.cpp
@@ -11,6 +11,16 @@
 #include <string>
 #include "network/packet.h"
 
+#ifndef htonll
+#define htonll(x)                                                                                  \
+    ((1 == htonl(1)) ? (x) : ((uint64_t)htonl((x)&0xFFFFFFFF) << 32) | htonl((x) >> 32))
+#endif
+
+#ifndef ntohll
+#define ntohll(x)                                                                                  \
+    ((1 == ntohl(1)) ? (x) : ((uint64_t)ntohl((x)&0xFFFFFFFF) << 32) | ntohl((x) >> 32))
+#endif
+
 namespace Network {
 
 #ifndef htonll
